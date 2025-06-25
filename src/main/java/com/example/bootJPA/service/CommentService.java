f81113a3e23ac6467b63d83961b40437116aec70
Msk117 <alstb0117@gmail.com>
2025-06-25 17:16:56 +0900

    250625_board_comment

---
+++ src/main/java/com/example/bootJPA/service/CommentService.java
@@ -0,0 +1,43 @@
+package com.example.bootJPA.service;
+
+
+import com.example.bootJPA.dto.CommentDTO;
+import com.example.bootJPA.entity.Comment;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface CommentService {
+    /* convert */
+    // DTO => entity
+    default Comment convertDtoToEntity(CommentDTO commentDTO) {
+        return Comment.builder()
+                .cno(commentDTO.getCno())
+                .bno(commentDTO.getBno())
+                .writer(commentDTO.getWriter())
+                .content(commentDTO.getContent())
+                .build();
+    }
+
+    // entity => DTO
+    default CommentDTO convertEntityToDto(Comment comment){
+        return CommentDTO.builder()
+                .cno(comment.getCno())
+                .bno(comment.getBno())
+                .writer(comment.getWriter())
+                .content(comment.getContent())
+                .regDate(comment.getRegDate())
+                .modDate(comment.getModDate())
+                .build();
+    }
+
+
+    long post(CommentDTO commentDTO);
+    Page<CommentDTO> getList(Long bno, int page);
+
+
+    Long remove(long cno);
+
+
+    Long modify(CommentDTO commentDTO);
+}
